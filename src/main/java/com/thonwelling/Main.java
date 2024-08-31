13d8e832b6cb3ee836454defe8413dc0ab583bb4
Thonwelling <thonwellingdani13@gmail.com>
2024-08-31 15:59:02 -0300

    Implementando inicio do jogo

--- src/main/java/com/thonwelling/Main.java
+++ src/main/java/com/thonwelling/Main.java
@@ -1,7 +1,16 @@
 package com.thonwelling;
 
+import com.thonwelling.models.Table;
+
 public class Main {
   public static void main(String[] args) {
+
     System.out.println("Hello world!");
+    Table table = new Table(6, 6, 6);
+
+    table.openField(3, 3);
+    table.changeMarcation(4,4);
+    table.changeMarcation(4,5);
+    System.out.println(table);
   }
 }

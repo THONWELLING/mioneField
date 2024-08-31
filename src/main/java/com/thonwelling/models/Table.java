1b1d8319d89d19ffa96190c7872c53ad710f3c3a
Thonwelling <thonwellingdani13@gmail.com>
2024-08-31 15:57:20 -0300

    Implementa Métodos  openField, changeMarcation, isFieldMarked e isFieldOpened

--- src/main/java/com/thonwelling/models/Table.java
+++ src/main/java/com/thonwelling/models/Table.java
@@ -6,19 +6,32 @@ import java.util.function.Predicate;
 
 public class Table {
 
-private int rowsQuantity;
-private int columnsQuantity;
+  private int rowsQuantity;
+  private int columnsQuantity;
   private int minesQuantity;
   private final List<Field> fields = new ArrayList<>();
 
-  public Table(int rowsQuantity, int columnsQuantity, int minesQuantity) {
-    this.rowsQuantity = rowsQuantity;
-    this.columnsQuantity = columnsQuantity;
-    this.minesQuantity = minesQuantity;
+  public Table(int pRowsQuantity, int pColumnsQuantity, int pMinesQuantity) {
+    this.rowsQuantity    = pRowsQuantity;
+    this.columnsQuantity = pColumnsQuantity;
+    this.minesQuantity   = pMinesQuantity;
 
     generateFields();
     associteNeighbors();
     sortMines();
+  }
+
+  public void openField(int pRow, int pColumn){
+    fields.parallelStream()
+            .filter(field -> field.getRow() == pRow && field.getColumn() ==pColumn)
+            .findFirst()
+            .ifPresent(field -> field.open());
+  }
+  public void changeMarcation(int pRow, int pColumn){
+    fields.parallelStream()
+            .filter(field -> field.getRow() == pRow && field.getColumn() ==pColumn)
+            .findFirst()
+            .ifPresent(field -> field.toggleMarkup());
   }
   private void generateFields() {
     for (int row = 0; row < rowsQuantity; row++) {
@@ -54,4 +67,36 @@ private int columnsQuantity;
     sortMines();
   }
 
+  public boolean isFieldMarked (int pRow, int pColumn) {
+    return fields.parallelStream()
+            .filter(field -> field.getRow() == pRow && field.getColumn() == pColumn)
+            .findFirst()
+            .map(Field::isMarked)
+            .orElse(false);
+  }
+  public boolean isFieldOpened (int pRow, int pColumn) {
+    return fields.parallelStream()
+            .filter(field -> field.getRow() == pRow && field.getColumn() == pColumn)
+            .findFirst()
+            .map(Field::isMarked)
+            .orElse(false);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder stringBuilder = new StringBuilder();
+    int i = 0;
+    for (int r = 0; r < rowsQuantity; r++) {
+      for (int c = 0; c < columnsQuantity; c++) {
+        stringBuilder.append(" ");
+        stringBuilder.append(fields.get(i));
+        stringBuilder.append(" ");
+        i++;
+
+      }
+      stringBuilder.append("\n");
+
+    }
+    return stringBuilder.toString();
+  }
 }
